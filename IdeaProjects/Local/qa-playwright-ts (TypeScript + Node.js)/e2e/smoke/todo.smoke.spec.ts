3303bfca7110d0cc09a841dc421813156528f23d
Daniel Bastián Nilo Palacios <91704142+D1nilo@users.noreply.github.com>
2025-11-05 01:55:29 -0300

    Delete IdeaProjects directory

--- IdeaProjects/Local/qa-playwright-ts (TypeScript + Node.js)/e2e/smoke/todo.smoke.spec.ts
+++
@@ -1,9 +0,0 @@
-import { test, expect } from '../../src/fixtures/test-base';
-
-test.describe('TodoMVC - Smoke', () => {
-  test('@smoke crear una tarea', async ({ todo }) => {
-    await todo.open();
-    await todo.addTask('Tarea 1');
-    await todo.verifyTaskCount(1);
-  });
-});
